972bc6a564cb7378bba8d5f5b7f734e5f7289b40
William <whteoh1352@gmail.com>
2022-05-23 21:36:44 +0800

    create task module and listing module are partially done

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { TaskFormPage } from '../task-form/task-form.page';
+import { DataProviderService } from '../services/data-provider.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
-  constructor(private modalController: ModalController) { }
+  public OnGoingTasks: any = []
+  public Urgent_Important_Tasks: any = []
+  public Urgent_Not_Important_Tasks: any = []
+  public Not_Urgent_Important_Tasks: any = []
+  public Not_Urgent_Not_Important_Tasks: any = []
+
+  constructor(
+    private modalController: ModalController,
+    private dataProvider: DataProviderService
+  ) {
+    this.requestFetchTasks()
+  }
+
+  async requestFetchTasks() {
+    const resp: any = await this.dataProvider.Tasks_ViewAllTasks();
+    try {
+      if (typeof resp !== "undefined" && Array.isArray(resp.Result) && resp.Result.length > 0) {
+        this.OnGoingTasks = resp.Result.filter(el => el.isComplete == 0)
+
+        this.Urgent_Important_Tasks = this.OnGoingTasks.filter(el => el.TaskPriorityID == 1)
+        this.Urgent_Not_Important_Tasks = this.OnGoingTasks.filter(el => el.TaskPriorityID == 2)
+        this.Not_Urgent_Important_Tasks = this.OnGoingTasks.filter(el => el.TaskPriorityID == 3)
+        this.Not_Urgent_Not_Important_Tasks = this.OnGoingTasks.filter(el => el.TaskPriorityID == 4)
+      }
+    }
+    catch {
+      this.OnGoingTasks = []
+      this.Urgent_Important_Tasks = []
+      this.Urgent_Not_Important_Tasks = []
+      this.Not_Urgent_Important_Tasks = []
+      this.Not_Urgent_Not_Important_Tasks = []
+    }
+  }
 
   async createNewTask() {
     const modal = await this.modalController.create({
@@ -19,8 +53,7 @@ export class Tab2Page {
       swipeToClose: true,
     })
     await modal.present();
-
     // const { data } = await modal.onWillDismiss();
-
   }
+
 }
--- src/app/task-form/task-form.page.ts
+++ src/app/task-form/task-form.page.ts
@@ -2,15 +2,37 @@ import { ModalController } from '@ionic/angular';
 import { Component, OnInit, Input } from '@angular/core';
 import { DataProviderService } from '../services/data-provider.service';
 
+interface TaskForm {
+  TaskID: number,
+  TaskName: string,
+  Notes: string,
+  PriorityType: any,
+
+  Invalid_TaskName: boolean,
+  Invalid_Priority: boolean,
+}
+
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.page.html',
   styleUrls: ['./task-form.page.scss'],
 })
+
 export class TaskFormPage implements OnInit {
-  // class variables
+  @Input() TaskObject
+
   public PageTitle = "Create";
   public PriorityTypes: any = []
+  public form: TaskForm = {
+    TaskID: null,
+    TaskName: '',
+    Notes: '',
+    PriorityType: null,
+
+    Invalid_TaskName: null,
+    Invalid_Priority: null,
+  }
+  public isInValid: boolean = true
 
   constructor(
     private modalController: ModalController,
@@ -18,11 +40,9 @@ export class TaskFormPage implements OnInit {
 
   ) { }
 
-  @Input() TaskObject
 
   ngOnInit() {
     this.PageTitle = (typeof this.TaskObject !== "undefined" && this.TaskObject !== null) ? "Edit" : "Create"
-
     this.requestFetchPriorityTypes();
   }
 
@@ -38,8 +58,36 @@ export class TaskFormPage implements OnInit {
       this.PriorityTypes = []
   }
 
-  submitForm() {
+  FormValidator(formEl, value) {
+    // check task name is empty
+    if(formEl === "TaskName")
+      this.form.Invalid_TaskName = (typeof value === "string" && value.trim() !== "") ? false : true
 
+    // check task priority is empty
+    if(formEl === "PriorityType")
+      this.form.Invalid_Priority = (typeof value !== "undefined" && value !== null && !isNaN(value)) ? false : true
+
+    if (!this.form.Invalid_TaskName && !this.form.Invalid_Priority)
+      this.isInValid = false
+    else
+      this.isInValid = true
+  }
+
+  async submitForm() {
+    // define submission parameters for validation before calling the API
+    let object = {
+      TASKNAME: this.form.TaskName,
+      NOTES: this.form.Notes,
+      PRIORTYTYPE: this.form.PriorityType
+    }
+
+    if (!this.isInValid) {
+      object.NOTES = object.NOTES !== "" && object.NOTES.trim() !== "" ? object.NOTES : "-"
+      const resp = await this.dataProvider.Tasks_CreateTask(object.TASKNAME, object.NOTES, object.PRIORTYTYPE)
+      console.log(resp)
+
+      
+    }
   }
 
   async dismissModal() {
